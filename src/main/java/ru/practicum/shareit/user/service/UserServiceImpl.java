abddafc618ce7f65ce83882735a1d0ae750f58e5
Elena <Alcabarena2006@ya.ru>
2023-05-08 15:18:03 +0300

    ТЗ 13

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -22,31 +22,43 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto create(UserDto userDto) {
-        validateEmail(userDto);
         User user = UserMapper.toUser(userDto);
-        return UserMapper.toUserDto(userRepository.create(user));
+        checkUniqueEmail(user);
+        userDto.setId(userRepository.create(user).getId());
+        return userDto;
     }
 
-     @Override
-     public UserDto update(Long userId, UserDto userDto) {
-         User user = userRepository.getUser(userId).orElseThrow(() ->
-                 new NotFoundException("Неверный идентификатор пользователя"));
+    private void checkUniqueEmail(User user) {
+        List<String> userEmails = userRepository.getAllUsers().values().stream()
+                .map(User::getEmail)
+                .collect(Collectors.toList());
+        if (userEmails.contains(user.getEmail())) {
+            throw new ValidationException(
+                    String.format("Пользователь с email = '%s' уже существует", user.getEmail()));
+        }
+    }
 
-                 if (userDto.getName() != null) {
-                     user.setName(userDto.getName());
-                 } else user.setName(user.getName());
+    @Override
+    public UserDto update(Long userId, UserDto userDto) {
+        userDto.setId(userId);
+        User userOld = userRepository.getUser(userId).orElseThrow(() ->
+                new NotFoundException("Неверный идентификатор пользователя"));
+        User userNew = UserMapper.toUser(userDto);
+        userNew = updateUserData(userNew, userOld);
+        return UserMapper.toUserDto(userRepository.update(userNew));
+    }
 
-                 if (userDto.getEmail() != null) {
-                     if (userRepository.getAll().stream()
-                             .filter(user1 -> !user1.getId().equals(userDto.getId()))
-                             .anyMatch(user1 -> userDto.getEmail().equals(user1.getEmail()))) {
-                         throw new ValidationException("Такой email уже используется");
-                     }
-                     user.setEmail(userDto.getEmail());
-                 }
-                 userRepository.update(user);
-                 return UserMapper.toUserDto(user);
-     }
+    private User updateUserData(User userNew, User userOld) {
+        if (userNew.getName() != null) {
+            userOld.setName(userNew.getName());
+        }
+        if (userNew.getEmail() != null) {
+            checkUniqueEmail(userNew);
+            userOld.setEmail(userNew.getEmail());
+        }
+
+        return userOld;
+    }
 
     @Override
     public List<UserDto> getAll() {
@@ -68,11 +80,10 @@ public class UserServiceImpl implements UserService {
         return UserMapper.toUserDto(user);
     }
 
-    public void validateEmail(UserDto userDto) {
+   /* public void validateEmail(UserDto userDto) {
         if (userRepository.getAll().stream()
-                .filter(user -> !user.getId().equals(userDto.getId()))
                 .anyMatch(user -> userDto.getEmail().equals(user.getEmail()))) {
             throw new ValidationException("Такой email уже используется");
         }
-    }
+    }*/
 }
