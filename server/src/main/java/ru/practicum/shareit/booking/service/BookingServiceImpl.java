c12fd3e7ec61e07b70c7dd2baa49c61ca7da83b8
Elena <Alcabarena2006@ya.ru>
2023-06-17 17:38:30 +0300

    ТЗ 16 Check Style добавила пробелы

--- server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -40,9 +40,10 @@ public class BookingServiceImpl implements BookingService {
         if (bookingDtoSimple.getStart().isBefore(LocalDateTime.now())) {
             throw new ValidationException("Время начала не может быть в прошлом");
         }
-        if (bookingDtoSimple.getEnd()==null) {
+        if (bookingDtoSimple.getEnd() == null) {
             throw new ValidationException("Время окончания не может быть равно нулю");
         }
+
         Booking booking = BookingMapper.fromSimpleToBooking(bookingDtoSimple);
 
         booking.setBooker(userRepository.findById(userId).orElseThrow());
